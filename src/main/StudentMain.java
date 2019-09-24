9f19341fd9fffb271fd3ee59c5da82fdd120a98d
divaibhav <divaibhav@gmail.com>
2019-09-24 14:43:47 +0530

    created list of student. implemented InvalidStudentException class which is a custom exception class

--- src/main/StudentMain.java
+++ src/main/StudentMain.java
@@ -2,14 +2,34 @@ package main;
 
 import student.Student;
 
+import java.util.Scanner;
+
 public class StudentMain {
     public static void main(String[] args) {
-
+        /*
         Student obj = new Student();
         obj.setName("vaibhav");
         obj.setRollNo(81);
         obj.setCpi(10.0);
         System.out.println(obj);
+
+         */
+
+        //creating list of student
+        Student[] list = new Student[5];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < list.length; i++) {
+            list[i] = new Student();
+            System.out.println("enter name");
+            list[i].setName(sc.nextLine());
+            System.out.println("enter rollno");
+            list[i].setRollNo(sc.nextInt());
+            System.out.println("enter cpi");
+            list[i].setCpi(sc.nextDouble());
+            sc.nextLine();
+
+        }
+
 
     }
     public Student search(Student[] list, int rollNo){
